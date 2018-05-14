d7fd790695716993ca4e9ca1dc9a471d94aa4512
Philipp Czora <philipp.czora@cloudogu.com>
2018-05-14 10:50:20 +0200

    #8190 Removed unnecessary creation of bash completion dir

--- build/make/package-debian.mk
+++ build/make/package-debian.mk
@@ -21,7 +21,6 @@ $(DEBIAN_PACKAGE): compile $(TARGET_DIR)/debian-binary prepare-package
 	@tar cvzf $(DEBIAN_TARGET_DIR)/control.tar.gz -C $(DEBIAN_TARGET_DIR)/control  --owner=0 --group=0 .
 # populating data directory
 	@install -m 0755 -d $(DEBIAN_TARGET_DIR)/data/usr/sbin
-	@install -m 0755 -d $(DEBIAN_TARGET_DIR)/data/etc/bash_completion.d/
 
 	@for dir in $$(find deb -mindepth 1 -not -name "DEBIAN" -a -type d |sed s@"^deb/"@"$(DEBIAN_TARGET_DIR)/data/"@) ; do install -m 0755 -d $${dir} ; done
 	@for file in $$(find deb -mindepth 1 -type f | grep -v "DEBIAN") ; do install -m 0644 $${file} $(DEBIAN_TARGET_DIR)/data/$${file#deb/}; done
