eeb2b32952b1ee28b4364a5616010a347edf3e1d
Philipp Czora <philipp.czora@cloudogu.com>
2018-05-14 15:02:24 +0200

    #8190 Only add binary to deb package iff there is a binary

--- build/make/package-debian.mk
+++ build/make/package-debian.mk
@@ -19,12 +19,17 @@ $(DEBIAN_PACKAGE): compile $(TARGET_DIR)/debian-binary prepare-package
 
 # creating control.tar.gz
 	@tar cvzf $(DEBIAN_TARGET_DIR)/control.tar.gz -C $(DEBIAN_TARGET_DIR)/control  --owner=0 --group=0 .
-# populating data directory
-	@install -m 0755 -d $(DEBIAN_TARGET_DIR)/data/usr/sbin
 
+# populating data directory
 	@for dir in $$(find deb -mindepth 1 -not -name "DEBIAN" -a -type d |sed s@"^deb/"@"$(DEBIAN_TARGET_DIR)/data/"@) ; do install -m 0755 -d $${dir} ; done
 	@for file in $$(find deb -mindepth 1 -type f | grep -v "DEBIAN") ; do install -m 0644 $${file} $(DEBIAN_TARGET_DIR)/data/$${file#deb/}; done
-	@install -m 0755 $(TARGET_DIR)/$(ARTIFACT_ID) $(DEBIAN_TARGET_DIR)/data/usr/sbin/
+
+# Copy binary to /usr/sbin, if it exists
+	@if [ -f $(TARGET_DIR)/$(ARTIFACT_ID) ]; then \
+		echo "Copying binary to /usr/sbin"; \
+		install -m 0755 -d $(DEBIAN_TARGET_DIR)/data/usr/sbin; \
+		install -m 0755 $(TARGET_DIR)/$(ARTIFACT_ID) $(DEBIAN_TARGET_DIR)/data/usr/sbin/; \
+	fi
 
 # creating data.tar.gz
 	@tar cvzf $(DEBIAN_TARGET_DIR)/data.tar.gz -C $(DEBIAN_TARGET_DIR)/data --owner=0 --group=0 .
