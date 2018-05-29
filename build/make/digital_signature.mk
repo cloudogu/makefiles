05633ba0b15c8ed2172a53ea8e149d711a2ccb2f
Maren Süwer <maren.suewer@cloudogu.com>
2018-05-29 15:43:05 +0200

    use silent running of commands

--- build/make/digital_signature.mk
+++ build/make/digital_signature.mk
@@ -3,12 +3,13 @@
 digital_signature: preparartion creating_checksum generating_signature
 
 preparartion:
-	rm -f $(TARGET_DIR)/Checksums.asc $(TARGET_DIR)/Checksums.sha256sum
+	@rm -f $(TARGET_DIR)/Checksums.asc $(TARGET_DIR)/Checksums.sha256sum
 
 creating_checksum:
-	echo "Generating Checksums"
-	$(foreach file,$(wildcard $(TARGET_DIR)/*), shasum -a 256 $(file) >> $(TARGET_DIR)/Checksums.sha256sum;)
+	@echo "Generating Checksums"
+	@$(foreach file,$(wildcard $(TARGET_DIR)/*), shasum -a 256 $(file) >> $(TARGET_DIR)/Checksums.sha256sum;)
 
 generating_signature:
-	echo "Generating Signature"
-	gpg --detach-sign -o $(TARGET_DIR)/Checksums.asc $(TARGET_DIR)/Checksums.sha256sum
+	@echo "Generating Signature"
+	@gpg --detach-sign -o $(TARGET_DIR)/Checksums.asc $(TARGET_DIR)/Checksums.sha256sum
+
