8d48e1c440f03b6485c4f325620c9f72d379f0c3
Sebastian Sdorra <s.sdorra@gmail.com>
2019-07-26 08:06:04 +0200

    remove duplicated code for unit testing
    use pre and post targets to start and stop docker-compose

--- build/make/unit-test-docker-compose.mk
+++ build/make/unit-test-docker-compose.mk
@@ -1,21 +1,15 @@
-XUNIT_XML=target/unit-tests.xml
-
-.PHONY: unit-test
-unit-test: ${XUNIT_XML}
-
-${XUNIT_XML}:
-	@mkdir -p $(TARGET_DIR)
+PRE_UNITTESTS?=start-docker-compose
+.PHONY: start-docker-compose
+start-docker-compose:
 	@if [ X"${ENVIRONMENT}" = X"local" ] ; then \
 		docker-compose up -d; \
 	fi;
-	@go test -v $(PACKAGES) | tee target/unit-tests.log
-	@if grep '^FAIL' target/unit-tests.log; then \
-		exit 1; \
-	fi
-	@cat target/unit-tests.log | go2xunit -output $@
+
+POST_UNITTESTS?=start-docker-compose
+.PHONY: stop-docker-compose
+stop-docker-compose:
 	@if [ X"${ENVIRONMENT}" = X"local" ] ; then \
 		docker-compose kill; \
 	fi;
 
-${GOPATH}/bin/go2xunit:
-	go get github.com/tebeka/go2xunit
+include build/make/unit-test.mk
