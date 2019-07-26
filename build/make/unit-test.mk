a96d40949d419ca98260c86dd92639968116972d
Sebastian Sdorra <s.sdorra@gmail.com>
2019-07-26 08:05:02 +0200

    calculate coverage for unit tests and introduce pre and post targets

--- build/make/unit-test.mk
+++ build/make/unit-test.mk
@@ -1,17 +1,27 @@
-XUNIT_XML=target/unit-tests.xml
+XUNIT_XML=$(TARGET_DIR)/unit-tests.xml
+COVERAGE_REPORT=$(TARGET_DIR)/coverage.out
+
+PRE_UNITTESTS?=
+POST_UNITTESTS?=
 
 .PHONY: unit-test
-unit-test: ${XUNIT_XML}
+unit-test: $(PRE_UNITTESTS) ${XUNIT_XML} $(POST_UNITTESTS)
 
-${XUNIT_XML}: ${GOPATH}/bin/go2xunit
-	mkdir -p $(TARGET_DIR)
-	go test -v $(PACKAGES) | tee target/unit-tests.log
+${XUNIT_XML}: ${GOPATH}/bin/go-junit-report
+	@mkdir -p $(TARGET_DIR)
+	@echo 'mode: set' > ${COVERAGE_REPORT}
+	@rm -f target/unit-tests.log || true
+	@for PKG in $(PACKAGES) ; do \
+    go test -v $$PKG -coverprofile=${COVERAGE_REPORT}.tmp 2>&1 | tee target/unit-tests.log.tmp ; \
+		cat ${COVERAGE_REPORT}.tmp | tail +2 >> ${COVERAGE_REPORT} ; \
+		rm -f ${COVERAGE_REPORT}.tmp ; \
+		cat target/unit-tests.log.tmp >> target/unit-tests.log ; \
+		rm -f target/unit-tests.log.tmp ; \
+	done
+	@cat target/unit-tests.log | go-junit-report > $@
 	@if grep '^FAIL' target/unit-tests.log; then \
 		exit 1; \
 	fi
-	@if grep '^=== RUN' target/unit-tests.log; then \
-	  cat target/unit-tests.log | go2xunit -output $@; \
-	fi
 
-${GOPATH}/bin/go2xunit:
-	go get github.com/tebeka/go2xunit
+${GOPATH}/bin/go-junit-report:
+	@go get -u github.com/jstemmer/go-junit-report
