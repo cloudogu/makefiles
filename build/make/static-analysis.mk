2e6af15690453da717980a201a825d80ff3c11f2
Philipp Pixel <pixeloffice@philipppixel.de>
2020-12-17 08:19:35 +0100

    #51 Refactor golangci-lint and reviewdog calls with subtler target structure
    This commit formulates the static-analysis call structure in a smarter, more
    `make`-like way by extracting and re-using targets.

--- build/make/static-analysis.mk
+++ build/make/static-analysis.mk
@@ -3,6 +3,8 @@ GOIMAGE?=golang
 GOTAG?=1.14.13
 CUSTOM_GO_MOUNT?=-v /tmp:/tmp
 
+REVIEW_DOG=$(TMP_DIR)/bin/reviewdog
+LINT=$(TMP_DIR)/bin/golangci-lint
 # ignore tests and mocks
 LINTFLAGS=--tests=false --skip-files="^.*_mock.go$$" --skip-files="^.*/mock.*.go$$"
 
@@ -10,16 +12,16 @@ LINTFLAGS=--tests=false --skip-files="^.*_mock.go$$" --skip-files="^.*/mock.*.go
 static-analysis: static-analysis-$(ENVIRONMENT)
 
 .PHONY: static-analysis-ci
-static-analysis-ci: $(TMP_DIR)/bin/golangci-lint
+static-analysis-ci:
 	@make $(STATIC_ANALYSIS_DIR)/static-analysis-cs.log $(STATIC_ANALYSIS_DIR)/static-analysis.log static-analysis-ci-report-pr
 
-static-analysis-ci-report-pr: $(TMP_DIR)/bin/reviewdog
+static-analysis-ci-report-pr:
 	@if [ X"$(CI_PULL_REQUEST)" != X"" -a X"$(CI_PULL_REQUEST)" != X"null" ] ; then \
-  		cat $(STATIC_ANALYSIS_DIR)/static-analysis-cs.log | CI_COMMIT=$(COMMIT_ID) $(TMP_DIR)/bin/reviewdog -f=checkstyle -reporter="github-pr-review"; \
+  		cat $(STATIC_ANALYSIS_DIR)/static-analysis-cs.log | CI_COMMIT=$(COMMIT_ID) $(REVIEW_DOG) -f=checkstyle -reporter="github-pr-review"; \
   	fi
 
 .PHONY: static-analysis-local
-static-analysis-local: $(PASSWD) $(ETCGROUP) $(HOME_DIR) $(TMP_DIR)/bin/golangci-lint
+static-analysis-local: $(PASSWD) $(ETCGROUP) $(HOME_DIR)
 	@docker run --rm \
 		-e GOOS=$(GOOS) \
 		-e GOARCH=$(GOARCH) \
@@ -33,27 +35,27 @@ static-analysis-local: $(PASSWD) $(ETCGROUP) $(HOME_DIR) $(TMP_DIR)/bin/golangci
 		$(GOIMAGE):$(GOTAG) \
 			make $(STATIC_ANALYSIS_DIR)/static-analysis-cs.log $(STATIC_ANALYSIS_DIR)/static-analysis.log static-analysis-ci-report-local
 
-$(STATIC_ANALYSIS_DIR)/static-analysis.log: $(STATIC_ANALYSIS_DIR) $(TMP_DIR)
+$(STATIC_ANALYSIS_DIR)/static-analysis.log: $(STATIC_ANALYSIS_DIR)
 	@echo ""
 	@echo "complete static analysis:"
 	@echo ""
-	@$(TMP_DIR)/bin/golangci-lint $(LINTFLAGS) run ./... | tee $@
+	@$(LINT) $(LINTFLAGS) run ./... | tee $@
 
-$(STATIC_ANALYSIS_DIR)/static-analysis-cs.log: $(STATIC_ANALYSIS_DIR) $(TMP_DIR)
+$(STATIC_ANALYSIS_DIR)/static-analysis-cs.log: $(STATIC_ANALYSIS_DIR)
 	@echo "run static analysis with export to checkstyle format"
-	@$(TMP_DIR)/bin/golangci-lint $(LINTFLAGS) run --out-format=checkstyle ./... > $@ | true
+	@$(LINT) $(LINTFLAGS) run --out-format=checkstyle ./... > $@ | true
 
-$(STATIC_ANALYSIS_DIR):
+$(STATIC_ANALYSIS_DIR): $(LINT)
 	@mkdir -p $(STATIC_ANALYSIS_DIR)
 
-static-analysis-ci-report-local: $(TMP_DIR)/bin/reviewdog
+static-analysis-ci-report-local: $(STATIC_ANALYSIS_DIR)/static-analysis-cs.log
 	@echo ""
 	@echo "differences to develop branch:"
 	@echo ""
-	@cat $(STATIC_ANALYSIS_DIR)/static-analysis-cs.log | $(TMP_DIR)/bin/reviewdog -f checkstyle -diff "git diff develop"
+	@cat $(STATIC_ANALYSIS_DIR)/static-analysis-cs.log | $(REVIEW_DOG) -f checkstyle -diff "git diff develop"
 
-$(TMP_DIR)/bin/golangci-lint:
+$(LINT): $(TMP_DIR)
 	@curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(TMP_DIR)/bin v1.33.0
 
-$(TMP_DIR)/bin/reviewdog:
+$(REVIEW_DOG): $(TMP_DIR)
 	@curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b $(TMP_DIR)/bin
