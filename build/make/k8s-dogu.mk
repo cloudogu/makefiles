98c1a2d0864cba8bb9bf0e190a02749b6d433975
Nico Franzeck <nico.franzeck@cloudogu.com>
2026-09-10 22:22:59 +0200

    [#273] Use immutable tag for dogu dev versions

--- build/make/k8s-dogu.mk
+++ build/make/k8s-dogu.mk
@@ -17,6 +17,19 @@ ifeq (${K8S_MK_INCLUDE_MARKER}, )
 	include ${BUILD_DIR}/make/k8s.mk
 endif
 
+# Immutable dev tags: when STAGE=development every `make build` produces a unique, non-overwriting image tag.
+DOGU_BUILD_VERSION := $(shell date +%s)
+DOGU_DEV_VERSION ?= $(VERSION).dev.$(DOGU_BUILD_VERSION)
+ifeq (${STAGE}, development)
+	DOGU_TARGET_VERSION = $(DOGU_DEV_VERSION)
+else
+	DOGU_TARGET_VERSION = $(VERSION)
+endif
+# Dogu-scoped override of the shared dev image tags from k8s.mk so only dogu builds get immutable tags.
+IMAGE_DEV_VERSION = $(IMAGE_DEV):$(DOGU_TARGET_VERSION)
+IMAGE_DEV_PUSH_VERSION = $(IMAGE_DEV_PUSH):$(DOGU_TARGET_VERSION)
+$(info DOGU_TARGET_VERSION=$(DOGU_TARGET_VERSION))
+
 ##@ K8s - EcoSystem
 
 .PHONY: build
@@ -34,7 +47,7 @@ K8S_RESOURCE_DOGU ?= $(K8S_RESOURCE_TEMP_FOLDER)/$(ARTIFACT_ID).yaml
 create-dogu-resource: ${BINARY_YQ} $(K8S_RESOURCE_TEMP_FOLDER)
 	@echo "Generating temporary K8s resources $(K8S_RESOURCE_DOGU)..."
 	@rm -f $(K8S_RESOURCE_DOGU)
-	@sed "s|NAMESPACE|$(ARTIFACT_NAMESPACE)|g" $(K8S_RESOURCE_DOGU_CR_TEMPLATE_YAML) | sed "s|NAME|$(ARTIFACT_ID)|g"  | sed "s|VERSION|$(VERSION)|g" >> $(K8S_RESOURCE_DOGU)
+	@sed "s|NAMESPACE|$(ARTIFACT_NAMESPACE)|g" $(K8S_RESOURCE_DOGU_CR_TEMPLATE_YAML) | sed "s|NAME|$(ARTIFACT_ID)|g"  | sed "s|VERSION|$(DOGU_TARGET_VERSION)|g" >> $(K8S_RESOURCE_DOGU)
 	@echo "Done."
 
 .PHONY: apply-dogu-resource
@@ -46,6 +59,6 @@ apply-dogu-resource:
 .PHONY: install-dogu-descriptor
 install-dogu-descriptor: ${BINARY_YQ} $(TARGET_DIR) ## Installs a configmap with current dogu.json into the cluster.
 	@echo "Generate configmap from dogu.json..."
-	@$(BINARY_YQ) -oj ".Image=\"${IMAGE_DEV}\" | .Version=\"${VERSION}\""  ${DOGU_JSON_FILE} > ${DOGU_JSON_DEV_FILE}
+	@$(BINARY_YQ) -oj ".Image=\"${IMAGE_DEV}\" | .Version=\"${DOGU_TARGET_VERSION}\""  ${DOGU_JSON_FILE} > ${DOGU_JSON_DEV_FILE}
 	@kubectl --context="${KUBE_CONTEXT_NAME}" create configmap "$(ARTIFACT_ID)-descriptor" --from-file=$(DOGU_JSON_DEV_FILE) --dry-run=client -o yaml | kubectl --context="${KUBE_CONTEXT_NAME}" --namespace=${NAMESPACE} apply -f -
 	@echo "Done."
